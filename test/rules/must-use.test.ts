64a66633bf8095e43bf20be97cb987c4c579257e
Leon Si <leon@leondreamed.com>
2024-01-02 16:58:46 -0500

    fix

--- test/rules/must-use.test.ts
+++ test/rules/must-use.test.ts
@@ -1,77 +1,24 @@
 import { RuleTester } from '@typescript-eslint/rule-tester';
 import rule from '../../src/rules/must-use-result.js';
 import { MessageIds } from '../../src/utils.js';
+import fs from 'node:fs';
 import { outdent } from 'outdent';
 import path from 'pathe';
+
+const neverthrowTypes = fs.readFileSync(
+	path.join(__dirname, '../fixture/neverthrow.d.ts'),
+	'utf8'
+);
 
 function injectResult(name: string, text: string) {
 	return (
 		outdent({ trimTrailingNewline: false })`
 			// ${name}
-			declare interface ErrorConfig {
-				withStackTrace: boolean;
-			}
-			declare type InferOkTypes<R> = R extends Result<infer T, unknown> ? T : never;
-			declare type InferErrTypes<R> = R extends Result<unknown, infer E> ? E : never;
-			declare type Result<T, E> = Ok<T, E> | Err<T, E>;
-			interface IResult<T, E> {
-				isOk(): this is Ok<T, E>;
-				isErr(): this is Err<T, E>;
-				map<A>(f: (t: T) => A): Result<A, E>;
-				mapErr<U>(f: (e: E) => U): Result<T, U>;
-				andThen<R extends Result<unknown, unknown>>(f: (t: T) => R): Result<InferOkTypes<R>, InferErrTypes<R> | E>;
-				andThen<U, F>(f: (t: T) => Result<U, F>): Result<U, E | F>;
-				orElse<R extends Result<unknown, unknown>>(f: (e: E) => R): Result<T, InferErrTypes<R>>;
-				orElse<A>(f: (e: E) => Result<T, A>): Result<T, A>;
-				asyncAndThen<U, F>(f: (t: T) => ResultAsync<U, F>): ResultAsync<U, E | F>;
-				asyncMap<U>(f: (t: T) => Promise<U>): ResultAsync<U, E>;
-				unwrapOr<A>(v: A): T | A;
-				match<A>(ok: (t: T) => A, err: (e: E) => A): A;
-				_unsafeUnwrap(config?: ErrorConfig): T;
-				_unsafeUnwrapErr(config?: ErrorConfig): E;
-			}
-
-			declare class Ok<T, E> implements IResult<T, E> {
-				readonly value: T;
-				constructor(value: T);
-				isOk(): this is Ok<T, E>;
-				isErr(): this is Err<T, E>;
-				map<A>(f: (t: T) => A): Result<A, E>;
-				mapErr<U>(_f: (e: E) => U): Result<T, U>;
-				andThen<R extends Result<unknown, unknown>>(f: (t: T) => R): Result<InferOkTypes<R>, InferErrTypes<R> | E>;
-				andThen<U, F>(f: (t: T) => Result<U, F>): Result<U, E | F>;
-				orElse<R extends Result<unknown, unknown>>(_f: (e: E) => R): Result<T, InferErrTypes<R>>;
-				orElse<A>(_f: (e: E) => Result<T, A>): Result<T, A>;
-				asyncAndThen<U, F>(f: (t: T) => ResultAsync<U, F>): ResultAsync<U, E | F>;
-				asyncMap<U>(f: (t: T) => Promise<U>): ResultAsync<U, E>;
-				unwrapOr<A>(_v: A): T | A;
-				match<A>(ok: (t: T) => A, _err: (e: E) => A): A;
-				_unsafeUnwrap(_?: ErrorConfig): T;
-				_unsafeUnwrapErr(config?: ErrorConfig): E;
-			}
-
-			declare class Err<T, E> implements IResult<T, E> {
-				readonly error: E;
-				constructor(error: E);
-				isOk(): this is Ok<T, E>;
-				isErr(): this is Err<T, E>;
-				map<A>(_f: (t: T) => A): Result<A, E>;
-				mapErr<U>(f: (e: E) => U): Result<T, U>;
-				andThen<R extends Result<unknown, unknown>>(_f: (t: T) => R): Result<InferOkTypes<R>, InferErrTypes<R> | E>;
-				andThen<U, F>(_f: (t: T) => Result<U, F>): Result<U, E | F>;
-				orElse<R extends Result<unknown, unknown>>(f: (e: E) => R): Result<T, InferErrTypes<R>>;
-				orElse<A>(f: (e: E) => Result<T, A>): Result<T, A>;
-				asyncAndThen<U, F>(_f: (t: T) => ResultAsync<U, F>): ResultAsync<U, E | F>;
-				asyncMap<U>(_f: (t: T) => Promise<U>): ResultAsync<U, E>;
-				unwrapOr<A>(v: A): T | A;
-				match<A>(_ok: (t: T) => A, err: (e: E) => A): A;
-				_unsafeUnwrap(config?: ErrorConfig): T;
-				_unsafeUnwrapErr(_?: ErrorConfig): E;
-			}
-
+			${neverthrowTypes}
 			declare function getResult(): Result<string, Error>
+			declare function getResultAsync(): ResultAsync<string, Error>
 			declare function getNormal(): number
-			const obj: { get: () => Result<string, Error> }
+			const obj: { get: () => Result<string, Error>, getAsync: () => ResultAsync<string, Error> }
 		` + text
 	);
 }
@@ -87,18 +34,23 @@ const ruleTester = new RuleTester({
 ruleTester.run('must-use-result', rule, {
 	valid: [
 		injectResult(
-			'call unwrapOr',
+			'call unwrap',
 			outdent`
 				const result = getResult()
-
-				result.unwrapOr()
+				result.unwrap()
+			`
+		),
+		injectResult(
+			'call ResultAsync unwrap',
+			outdent`
+				const resultAsync = getResultAsync()
+				resultAsync.unwrap()
 			`
 		),
 		injectResult(
 			'call unwrapOr after some methods',
 			outdent`
 				const result = getResult()
-
 				result.map(() => {}).unwrapOr('')
 			`
 		),
@@ -162,6 +114,24 @@ ruleTester.run('must-use-result', rule, {
 				'only call',
 				outdent`
 					getResult()
+				`
+			),
+			errors: [{ messageId: MessageIds.MUST_USE }]
+		},
+		{
+			code: injectResult(
+				'only ResultAsync call',
+				outdent`
+					await getResultAsync()
+				`
+			),
+			errors: [{ messageId: MessageIds.MUST_USE }]
+		},
+		{
+			code: injectResult(
+				'only await call',
+				outdent`
+					await getResult()
 				`
 			),
 			errors: [{ messageId: MessageIds.MUST_USE }]
